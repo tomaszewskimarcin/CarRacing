9c7c4b9a31cc649fad482802824d5a2cf343e0ea
tomaszewskimarcin <marcintomaszewski1993@gmail.com>
2017-07-05 22:59:08 +0200

    Working on judge and car agents

--- src/agents/Car.java
+++ src/agents/Car.java
@@ -1,5 +1,68 @@
 package agents;
 
+import java.awt.Point;
+
 public class Car {
 
+	private int number;
+	private String name;
+	private Point pos = new Point(0, 0);
+	private int spd = 2;
+	private int mode;
+	private int time = 0;
+	private int bestLap = 0;
+	
+	public Car(int number, String name, int x,int y){
+		this.number = number;
+		this.name = name;
+		mode = 1;
+		pos.setLocation(x, y);
+	}
+	
+	public void penalty(){
+		
+	}
+	
+	public int getNumber(){
+		return number;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getTime(){
+		return time;
+	}
+	
+	public int getBestLap(){
+		return bestLap;
+	}
+	
+	public void setMode(int mode){
+		if(mode>=1 && mode <=3){
+			this.mode = mode;
+		}
+	}
+	
+	public int getMode(){
+		return mode;
+	}
+	
+	public void setSpd(int spd){
+		this.spd = spd;
+	}
+	
+	public int getSpd(){
+		return spd;
+	}
+	
+	public void setPos(int x, int y){
+		pos.setLocation(x, y);
+	}
+	
+	public Point getPos(){
+		return pos;
+	}
+	
 }
--- src/agents/Judge.java
+++ src/agents/Judge.java
@@ -1,5 +1,66 @@
 package agents;
 
+import javax.swing.JOptionPane;
+
 public class Judge {
+	
+	private Car[] cars;
+	private Car penaltyCar;
+	private boolean penalty = true;
+	private int penaltyTime = 5;
+	private boolean race = false;
+	
+	
+	public Judge(){
+		checkForCars();
+		checkForPenaltys();
+	}
+	
+	private void checkForCars(){
+		
+	}
+	
+	private void checkForPenaltys(){
+		while(race){
+			if(penalty){
+				penaltyCar = cars[cars.length];
+				forcePenalty(penaltyCar, penaltyTime);
+			}
+		}
+	}
+	
+	private void forcePenalty(Car c, int time){
+		c.penalty();
+	}
+	
+	public void listStandings(){
+		String text = "";
+		Car[] standings = new Car[cars.length];
+		int i = 0;
+		for( Car c : cars){
+			standings[i] = c;
+		}
+		Car tmp;
+		for(int j = 0; j<standings.length; j++){
+			for( int x = 1; x<standings.length-1;x++){
+				Car c1 = standings[x];
+				Car c2 = standings[x-1];
+				if(c2.getTime() > c1.getTime()){
+					tmp = c2;
+					standings[x - 1] = standings[x];
+					standings[x] = tmp;
+				}
+			}
+		}
+		i = 0;
+		for( Car cr : standings){
+			if(i == 0){
+				text += "place	name(number)	time	bestLap";
+			}else{
+				text += "\n"+i+".	"+cr.getName()+"("+cr.getNumber()+")	"+cr.getTime()+"	"+cr.getBestLap();
+			}
+		}
+		JOptionPane.showMessageDialog(null, text);
+	}
 
 }
